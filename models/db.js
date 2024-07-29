776468544878e4676316bb520f66d549f3f27dcd
richardiit <166767441+richardiit@users.noreply.github.com>
2024-07-29 12:37:57 +0000

    Update db.js

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '#MSyql@123',
+  password: 'xxx',
   database: 'program_app_db2'
 });
 
